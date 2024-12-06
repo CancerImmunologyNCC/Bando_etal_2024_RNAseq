5e01907141f3b2914f09bc59adbc76fa489da394
CancerImmunology_NCC <60129580+CancerImmunologyNCC@users.noreply.github.com>
2024-12-06 14:53:19 +0900

    Update ssGSEA.R

--- codes/ssGSEA.R
+++ codes/ssGSEA.R
@@ -9,7 +9,7 @@ library(GSVA)
 
 ## import geneset
 ### https://www.nature.com/articles/s41591-020-1082-2 Sup.Table8 and Treg geneset from https://www.sciencedirect.com/science/article/pii/S0092867414016390
-geneset <- path/to/geneset/geneset.txt
+geneset <- "path/to/geneset/geneset.txt"
 geneset <- fread(geneset,header=FALSE)
 geneset <- rbind(geneset,data.frame("CD4_Regulatory_T_cells","FOXP3,LINC02694,IL5,CTLA4,IL32,GPR15,IL4"),use.names=FALSE)
 
@@ -18,7 +18,7 @@ gs <- as.list(geneset$genes) %>% str_split(pattern=",")
 names(gs) <- geneset$geneset
 
 ## import data
-TPM <- path/to/tpm/TPM.txt
+TPM <- "path/to/tpm/TPM.txt"
 TPM <- fread(TPM)
 
 ## data formatting
@@ -32,7 +32,7 @@ rownames(logTPM) <- TPM$Gene.Name
 results <- gsva(logTPM, gs, method=c("ssgsea"),verbose=FALSE)
 
 ## save results
-OUTPUT_PATH <- path/to/output
+OUTPUT_PATH <- "path/to/output"
 out <- results %>% data.frame()
 
 fwrite(out,paste0(OUTPUT_PATH,"/ssGSEA_results.txt"), row.names = TRUE,sep="\t")
